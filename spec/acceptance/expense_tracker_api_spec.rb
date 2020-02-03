965025b2a8003e8555e9af80148f29c8d1af621d
excavator <str_token@mail.ru>
2020-02-03 14:54:15 +0300

    records submitted expenses

---
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -0,0 +1,24 @@
+require 'rack/test'
+require 'json'
+require_relative '../../app/api'
+
+module ExpenseTracker
+  RSpec.describe 'Expense Tracker API' do
+    include Rack::Test::Methods
+
+    def app
+      ExpenseTracker::API.new
+    end
+
+    it 'records submitted expenses' do    
+
+      coffee = {
+        'payee' => 'Starbucks',
+        'amount' => 5.75,
+        'date' => '2020-01-02'
+      }
+
+      post '/expenses', JSON.generate(coffee)
+    end
+  end
+end
