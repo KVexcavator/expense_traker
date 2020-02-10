56dc3fb55515b29ba1fb2422db7ded20071cd82a
excavator <str_token@mail.ru>
2020-02-10 13:49:27 +0300

    refactoring code   unit test with mock

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -15,28 +15,25 @@ module ExpenseTracker
 
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
-        it 'returns the expense id' do
-          expense = {'some' => 'data'}
+        let(:expense) {{ 'some' => 'data' }}
 
+        before do
           allow(ledger).to receive(:record)
             .with(expense)
             .and_return(RecordResult.new(true, 417, nil))
-          
-          post '/expenses', JSON.generate(expense)
+        end 
 
+        it 'returns the expense id' do          
+          post '/expenses', JSON.generate(expense)
           parsed = JSON.parse(last_response.body)
           expect(parsed).to include('expense_id' => 417)
         end
-        it 'responds with a 200 (OK)' do
-          expense = {'some' => 'data'}
 
-          allow(ledger).to receive(:record)
-            .with(expense)
-            .and_return(RecordResult.new(true, 417, nil))
-          
+        it 'responds with a 200 (OK)' do
           post '/expenses', JSON.generate(expense)
           expect(last_response.status).to eq(200)
         end
+
       end
       context 'when the expense fails validation' do
         it 'returns an error massage'
